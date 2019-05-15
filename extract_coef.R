3146562821a12c1bb2a2ca28f28234598d730fd8
saidejp <said.ejp@gmail.com>
2019-05-15 16:00:20 -0500

    Selective Inference

--- extract_coef.R
+++ extract_coef.R
@@ -1,4 +1,23 @@
-# Extract coefficients 
+
+# Extract coefficients  ---------------------------------------------------
+
+
+# Packages ----------------------------------------------------------------
+
+pacman::p_load(caret, tidyverse, caTools, gridExtra, 
+               ranger, mlbench, AppliedPredictiveModeling,
+               plotROC, glmnet, selectiveInference)
+
+
+
+# Data --------------------------------------------------------------------
+
+load("adicciones.RData")
+
+
+
+# Betas -------------------------------------------------------------------
+
 coef(glmnet$finalModel, s = glmnet$bestTune$lambda) %>% 
   broom::tidy() %>% 
   mutate(row1 = reorder(row, value)) %>% 
@@ -8,11 +27,30 @@ coef(glmnet$finalModel, s = glmnet$bestTune$lambda) %>%
   coord_flip()
 
 
-betas <- 
-coef(glmnet$finalModel, s = glmnet$bestTune$lambda) %>% 
+betas <- coef(glmnet$finalModel, s = glmnet$bestTune$lambda) %>% 
   broom::tidy() %>% 
   arrange(desc(value)) %>% 
   mutate(value = round(value, 3))
 
 
-writexl::write_xlsx(betas, path = "~/Google Drive/Paper_EJN/betas.xlsx")
+
+# Selective Inference -----------------------------------------------------
+
+set.seed(43)
+n <- nrow(d)
+p <- ncol(d[-1])
+x <- as.matrix(d[-1])
+x <- scale(x, TRUE, TRUE)
+y <- ifelse(d$Grupo == "Adicto", 1, 0)
+
+# extract coef for a given lambda; note the 1/n factor!
+# (and here  we DO  include the intercept term)
+lambda <- glmnet$bestTune$lambda
+beta_hat <- coef(glmnet$finalModel, s = glmnet$bestTune$lambda)
+
+# compute fixed lambda p-values and selection intervals
+out <- fixedLassoInf(x, y, beta_hat, lambda, family = "binomial")
+out
+
+
+# writexl::write_xlsx(betas, path = "~/Google Drive/Paper_EJN/betas.xlsx")
