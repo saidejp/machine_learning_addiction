4455ad7c4bf0a15f3d0375ad331a7b422e4bf91d
saidejp <said.ejp@gmail.com>
2019-05-20 13:00:16 -0500

    Selective inference

--- extract_coef.R
+++ extract_coef.R
@@ -46,11 +46,10 @@ y <- ifelse(d$Grupo == "Adicto", 1, 0)
 # extract coef for a given lambda; note the 1/n factor!
 # (and here  we DO  include the intercept term)
 lambda <- glmnet$bestTune$lambda
-beta_hat <- coef(glmnet$finalModel, s = glmnet$bestTune$lambda)
+beta_hat <- coef(glmnet$finalModel, s = glmnet$bestTune$lambda/n)
 
 # compute fixed lambda p-values and selection intervals
 out <- fixedLassoInf(x, y, beta_hat, lambda, family = "binomial")
 out
 
 
-# writexl::write_xlsx(betas, path = "~/Google Drive/Paper_EJN/betas.xlsx")
